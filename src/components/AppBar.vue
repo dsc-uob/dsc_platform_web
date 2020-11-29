7a1d53ecb4a286d469c3aca264fe62b2c37fecca
Mohammed <redcodemohammed@gmail.com>
2020-11-30 02:06:49 +0300

    created AppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,39 +1,38 @@
 <template>
-	<v-app-bar app color="primary" dark>
-		<div class="d-flex align-center">
-			<v-img
-				alt="Vuetify Logo"
-				class="shrink mr-2"
-				contain
-				src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-				transition="scale-transition"
-				width="40"
-			/>
+	<div>
+		<v-app-bar>
+			<v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-			<v-img
-				alt="Vuetify Name"
-				class="shrink mt-1 hidden-sm-and-down"
-				contain
-				min-width="100"
-				src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-				width="100"
-			/>
-		</div>
+			<v-toolbar-title>Title</v-toolbar-title>
+		</v-app-bar>
 
-		<v-spacer></v-spacer>
+		<v-navigation-drawer v-model="drawer" absolute temporary>
+			<v-list nav dense>
+				<v-list-item-group v-model="group">
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-home</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>Home</v-list-item-title>
+					</v-list-item>
 
-		<v-btn
-			href="https://github.com/vuetifyjs/vuetify/releases/latest"
-			target="_blank"
-			text
-		>
-			<span class="mr-2">Latest Release</span>
-			<v-icon>mdi-open-in-new</v-icon>
-		</v-btn>
-	</v-app-bar>
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-account</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>Account</v-list-item-title>
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
+		</v-navigation-drawer>
+	</div>
 </template>
 <script>
 export default {
 	name: "AppBar",
+	data: () => ({
+		drawer: false,
+		group: null,
+	}),
 };
 </script>
